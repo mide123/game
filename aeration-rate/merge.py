4dce25c3bc3f980b8ffec62dcf2c0e6dfc755f89
mide123 <466767741@qq.com>
2022-10-24 23:01:56 +0800

    0.63

--- aeration-rate/merge.py
+++ aeration-rate/merge.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import gc
 
-xgboost_res = pd.read_csv("./res/xgboost_res.csv")
+xgboost_res = pd.read_csv("./res/catboost_res.csv")
 lightgbm_res = pd.read_csv("./res/lightgbm_res.csv")
 
 res = pd.DataFrame()
@@ -18,3 +18,10 @@ lightgbm_po = 0.65
 res[['Label1', 'Label2']] = lightgbm_po * lightgbm_res[['Label1', 'Label2']] + (1 - lightgbm_po) * xgboost_res[
     ['Label1', 'Label2']]
 res.to_csv("./res/merge_res.csv", index=False)
+
+# res = pd.DataFrame()
+# res['time'] = lightgbm_res['time']
+# res['Label1'] = lightgbm_res['Label1'] * 0.9 + 0.1 * xgboost_res['Label1']
+# res['Label2'] = lightgbm_res['Label2'] * 0.1 + 0.9 * xgboost_res['Label2']
+#
+# res.to_csv("./res/cross_res1.csv", index=False)
